a65b76ee6903a45a35dfb3dec47ee06438430708
jaime0010 <80421595+jaime0010@users.noreply.github.com>
2023-02-22 17:42:31 +0100

    funcion matrix

--- aporel10/aporel10/src/Peon.cpp
+++ aporel10/aporel10/src/Peon.cpp
@@ -3,8 +3,20 @@
 
 void Peon::dibuja()
 {
-
-	glutSolidSphere(1, 20, 20);
-	//glEnable(GL_TEXTURE_2D);
-	//glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("lib/imagenes/peon-blanco.png").id);
+    //ELEGIMOS COLOR DEL FONDO DE LA IMAGEN
+    if(color==BLANCO)
+        glColor3ub(255,255,255);
+    else
+        glColor3ub(0, 0,0);
+	
+	glEnable(GL_TEXTURE_2D);         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("lib/imagenes/reina-blancap.png").id);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+    glTexCoord2d(0, 1); ; glVertex3f( - 0.5,  - 0.5, 0.01f);
+    glTexCoord2d(1, 1); ; glVertex3f( + 0.5,  - 0.5, 0.01f);
+    glTexCoord2d(1, 0);   glVertex3f( + 0.5,  + 0.5, 0.01f);
+    glTexCoord2d(0, 0); glVertex3f(- 0.5,  + 0.5, 0.01f);
+    //
+    glEnd();
+    glEnable(GL_LIGHTING);
 }
--- aporel10/aporel10/src/Tablero.cpp
+++ aporel10/aporel10/src/Tablero.cpp
@@ -2,7 +2,15 @@
 
 Tablero::Tablero()
 {
+	for (int i = 0; i < filas; i++)
+	{
+		for (int j = 0; j < columnas; j++)
+		{
+			tab[i][j]=nullptr;
+		}
+	}
 	tab[2][2] = new Peon(Pieza::BLANCO) ;
+	tab[1][1] = new Peon(Pieza::NEGRO);
 
 }
 
@@ -37,8 +45,20 @@ void Tablero::dibuja()
 			glEnable(GL_LIGHTING);
 		}
 	}
+	for (int fila = 0; fila < filas; fila++)
+	{
+		for (int columna = 0; columna < columnas; columna++)
+		{
+			glPushMatrix();
+			glTranslatef(columna+0.5, fila+0.5, 0);
+			if(tab[fila][columna])tab[fila][columna]->dibuja();
+			glPopMatrix();
+		}
+	}
 	
-	tab[2][2]->dibuja();
+	
+	//tab[2][2]->dibuja();
+	//tab[1][1]->dibuja();
 }
 
 void Tablero::get_cor(int x, int y)
